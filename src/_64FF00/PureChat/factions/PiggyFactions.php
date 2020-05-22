a1875ddd841f4385a0d462afb71c85bbf160c9c5
Heisenburger69 <apkforgss@gmail.com>
2020-05-22 20:34:09 +0530

    Use role symbols rather than name

--- src/_64FF00/PureChat/factions/PiggyFactions.php
+++ src/_64FF00/PureChat/factions/PiggyFactions.php
@@ -10,12 +10,12 @@ class PiggyFactions implements FactionsInterface
 
     public function getAPI()
     {
-        return Server::getInstance()->getPluginManager()->getPlugin("PiggyFactions")->getPlayerManager();
+        return Server::getInstance()->getPluginManager()->getPlugin("PiggyFactions");
     }
 
     public function getPlayerFaction(Player $player)
     {
-        $member = $this->getAPI()->getPlayer($player->getUniqueId());
+        $member = $this->getAPI()->getPlayerManager()->getPlayer($player->getUniqueId());
         $faction = $member === null ? null : $member->getFaction();
         if($faction === null) {
             return "";
@@ -25,11 +25,11 @@ class PiggyFactions implements FactionsInterface
 
     public function getPlayerRank(Player $player)
     {
-        $member = $this->getAPI()->getPlayer($player->getUniqueId());
-        $faction = $member === null ? null : $member->getFaction();
-        if($faction === null) {
+        $member = $this->getAPI()->getPlayerManager()->getPlayer($player->getUniqueId());
+        $symbol = $member === null ? null : $this->getAPI()->getTagManager()->getPlayerRankSymbol($member);
+        if($member === null || $symbol === null) {
             return "";
         }
-        return $member->getRole();
+        return $symbol;
     }
 }
